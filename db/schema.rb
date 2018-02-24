ad81cd3f52bb41d62b577e507f1885f566f7eef0
dcamriff <driffelatl@gmail.com>
2018-02-24 15:30:07 -0500

    Added model Items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224200939) do
+ActiveRecord::Schema.define(version: 20180224202904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
